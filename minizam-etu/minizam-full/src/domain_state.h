3ef40676928e1e656fe5fab6090236e8321ce807
28709878 <danael.carbonneau@protonmail.com>
2024-03-12 13:46:13 +0100

    passage au global du stack pointer

--- minizam-etu/minizam-full/src/domain_state.h
+++ minizam-etu/minizam-full/src/domain_state.h
@@ -8,8 +8,8 @@ typedef struct _caml_domain_state {
   /* Stack */
   mlvalue* stack;
   mlvalue* heap;
-  int end_heap;
-
+  size_t end_heap;
+  size_t stack_pointer;
 } caml_domain_state;
 
 /* The global state */
@@ -17,5 +17,5 @@ extern caml_domain_state* Caml_state;
 
 /* Initialisation function for |Caml_state| */
 void caml_init_domain();
-void camml_free_domain(); //Ajout pour libérer correctement la mémoire du tas et de la pile
+void caml_free_domain(); //Ajout pour libérer correctement la mémoire du tas et de la pile
 #endif
